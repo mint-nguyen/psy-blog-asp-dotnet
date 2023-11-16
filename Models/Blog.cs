e9aed03f04f65ad38c90917c476ef1b71366e387
Mint <mintnguyen.pa@gmail.com>
2023-11-15 19:46:23 -0800

    feat: layout nav

--- Models/Blog.cs
+++ Models/Blog.cs
@@ -7,11 +7,12 @@ namespace MintPsyBlog.Models
 
         public int ID { get; set; }
         public string Title { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
 
-[Display(Name = "Release Date")]
+        [Display(Name = "Release Date")]
         [DataType(DataType.Date)]
         public DateTime ReleaseDate { get; set; }
         public string Category { get; set; } = string.Empty;
-        public decimal Price { get; set; }
+        public string Image { get; set; }
     }
 }
--- obj/Debug/net8.0/MintPsyBlog.AssemblyInfo.cs
+++ obj/Debug/net8.0/MintPsyBlog.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MintPsyBlog")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1afc7717cd1f60491c4ebaf62aaea5cd3e486701")]
 [assembly: System.Reflection.AssemblyProductAttribute("MintPsyBlog")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MintPsyBlog")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
