1afc7717cd1f60491c4ebaf62aaea5cd3e486701
Mint <mintnguyen.pa@gmail.com>
2023-11-15 19:10:33 -0800

    feat: add blog model

---
+++ Models/Blog.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MintPsyBlog.Models
+{
+    public class Blog
+    {
+
+        public int ID { get; set; }
+        public string Title { get; set; } = string.Empty;
+
+[Display(Name = "Release Date")]
+        [DataType(DataType.Date)]
+        public DateTime ReleaseDate { get; set; }
+        public string Category { get; set; } = string.Empty;
+        public decimal Price { get; set; }
+    }
+}
