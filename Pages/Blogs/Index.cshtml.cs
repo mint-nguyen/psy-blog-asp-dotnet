5e8df64b2b45e55304a8ae3aa0676902947e53b9
Mint <mintnguyen.pa@gmail.com>
2023-11-16 20:08:08 -0800

    feat: add search and filteration

--- Pages/Blogs/Index.cshtml.cs
+++ Pages/Blogs/Index.cshtml.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
 using MintPsyBlog.Data;
 using MintPsyBlog.Models;
+using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace MintPsyBlog.Pages.Blogs
 {
@@ -21,9 +22,33 @@ namespace MintPsyBlog.Pages.Blogs
 
         public IList<Blog> Blog { get;set; } = default!;
 
+        [BindProperty(SupportsGet = true)]
+        public string SearchString { get; set; }
+        public SelectList Categorys { get; set; }
+        [BindProperty(SupportsGet = true)]
+        public string BlogCategory { get; set; }
+
         public async Task OnGetAsync()
         {
-            Blog = await _context.Blog.ToListAsync();
+            // Use LINQ to get list of categorys.
+            IQueryable<string> categoryQuery = from m in _context.Blog
+                                            orderby m.Category
+                                            select m.Category;
+
+            var blogs = from m in _context.Blog
+                         select m;
+
+            if (!string.IsNullOrEmpty(SearchString))
+            {
+                blogs = blogs.Where(s => s.Title.Contains(SearchString));
+            }
+
+            if (!string.IsNullOrEmpty(BlogCategory))
+            {
+                blogs = blogs.Where(x => x.Category == BlogCategory);
+            }
+            Categorys = new SelectList(await categoryQuery.Distinct().ToListAsync());
+            Blog = await blogs.ToListAsync();
         }
     }
 }
